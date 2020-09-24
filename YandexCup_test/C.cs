2d2474482681cd50c4509f6343e639de037a3ad8
paulrwd85@gmail.com <paulrwd85@gmail.com>
2020-09-24 14:03:34 +0300

    C

--- YandexCup_test/C.cs
+++ YandexCup_test/C.cs
@@ -6,6 +6,66 @@ namespace YandexCup_test
 {
     class C
     {
+        static void Main(string[] args)
+        {
+            string[] first = Console.ReadLine().Split(' ');
 
+            int win = int.Parse(first[0]);
+            int count = int.Parse(first[1]);
+            int[] mas = new int[count];
+            int Vas = 0;
+            int Pet = 0;
+            bool Drw = true;
+
+            string[] second = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < count; i++)
+            {
+                mas[i] = int.Parse(second[i]);
+            }
+
+            foreach (int i in mas)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Pet++;
+                }
+                if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Vas++;
+                }
+                if (Pet == win && Vas == win)
+                {
+                    Console.WriteLine("Draw");
+                    Drw = false;
+                    break;
+                }
+                if (Pet == win)
+                {
+                    Console.WriteLine("Petya");
+                    Drw = false;
+                    break;
+                }
+                if (Vas == win)
+                {
+                    Console.WriteLine("Vasya");
+                    Drw = false;
+                    break;
+                }
+            }
+
+            if (Pet > Vas && Drw)
+            {
+                Console.WriteLine("Petya");
+            }
+            if (Vas > Pet && Drw)
+            {
+                Console.WriteLine("Vasya");
+            }
+            if (Pet == Vas && Drw)
+            {
+                Console.WriteLine("Draw");
+            }
+        }
     }
 }
