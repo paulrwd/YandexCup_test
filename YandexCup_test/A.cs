bfa22beefbcc9b7dd5d07d4b8724c63e953997c0
paulrwd85@gmail.com <paulrwd85@gmail.com>
2020-09-28 12:46:32 +0300

    2809_2

--- YandexCup_test/A.cs
+++ YandexCup_test/A.cs
@@ -6,5 +6,26 @@ namespace YandexCup_test
 {
     class A
     {
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine();
+        //    string j = Console.ReadLine();
+
+        //    Console.WriteLine();
+        //    string s = Console.ReadLine();
+
+        //    int result = 0;
+        //    foreach (char ch in s)
+        //    {
+        //        if (j.IndexOf(ch) >= 0)
+        //        {
+        //            ++result;
+        //        }
+        //    }
+
+        //    Console.WriteLine(result);
+
+
+        //}
     }
 }
--- YandexCup_test/Program.cs
+++ YandexCup_test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace YandexCup_test
 {
@@ -10,8 +11,9 @@ namespace YandexCup_test
 
             int count = int.Parse(first[0]);
             int period = int.Parse(first[1]);
-            int wakeup = int.Parse(first[2]);
+            int count_wakeup = int.Parse(first[2]);
             int[] mas = new int[count];
+            int res = 0;
            
             string[] second = Console.ReadLine().Split(' ');
 
@@ -20,7 +22,51 @@ namespace YandexCup_test
                 mas[i] = int.Parse(second[i]);                
             }
 
-          
+            mas = mas.OrderBy(x => x).ToArray();
+
+            int p = 0;
+            int c = 1;
+
+            if (count >= count_wakeup)
+            {
+               foreach (int i in mas)
+               {
+                  p++;
+                  if (p == count_wakeup)
+                  {
+                      res = i;
+                  }
+                        
+               }
+               
+            }
+            else
+            {
+                p = count - 1;
+                while (p != count_wakeup)
+                {
+                    foreach (int i in mas)
+                    {
+                        
+                        res = i + (c*period);
+                        p++;
+                        if (p == count_wakeup)
+                        {
+                            break;
+                        }
+                    }
+                    c++;
+
+                }
+                
+            }
+
+
+
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+
         }
     }
 }
