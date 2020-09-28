bb840b46829f0016190cb9a4a7a2a169d770111c
paulrwd85@gmail.com <paulrwd85@gmail.com>
2020-09-28 17:30:50 +0300

    2809_3

--- YandexCup_test/Program.cs
+++ YandexCup_test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace YandexCup_test
@@ -14,6 +15,7 @@ namespace YandexCup_test
             int count_wakeup = int.Parse(first[2]);
             int[] mas = new int[count];
             int res = 0;
+            List<int> mas_repeat = new List<int>();
            
             string[] second = Console.ReadLine().Split(' ');
 
@@ -26,47 +28,70 @@ namespace YandexCup_test
 
             int p = 0;
             int c = 1;
+            bool marker_rep = false;
 
-            if (count >= count_wakeup)
+            foreach (int i in mas)
             {
-               foreach (int i in mas)
+                marker_rep = false;
+                foreach(int j in mas_repeat)
+                {
+                    if(j == i)
+                    {
+                        marker_rep = true;
+                    }
+                }
+                if (marker_rep == false)
+                {
+                    mas_repeat.Add(i);
+                }
+            }
+
+
+            if (mas_repeat.Count >= count_wakeup)
+            {
+               foreach (int i in mas_repeat)
                {
                   p++;
-                  if (p == count_wakeup)
+                  if (p >= count_wakeup)
                   {
                       res = i;
-                  }
-                        
-               }
-               
+                      break;
+                  }                        
+               }               
             }
             else
             {
-                p = count - 1;
-                while (p != count_wakeup)
+                p = mas_repeat.Count;
+                while (p < count_wakeup)
                 {
                     foreach (int i in mas)
                     {
-                        
+                        marker_rep = false;
                         res = i + (c*period);
-                        p++;
-                        if (p == count_wakeup)
+                        
+                        foreach(int j in mas_repeat)
+                        {
+                            if(j == res)
+                            {
+                                marker_rep = true;
+                            }
+                        }
+                            if(marker_rep == false)
+                        {
+                            mas_repeat.Add(res);
+                            p++;
+                        }
+                         
+                        if (p >= count_wakeup)
                         {
                             break;
                         }
                     }
                     c++;
-
-                }
-                
+                }                
             }
-
-
-
-
             Console.WriteLine(res);
             Console.ReadLine();
-
         }
     }
 }
