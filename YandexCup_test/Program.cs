dcd5a0b68abd2b81bd5bf164f1fa8ee305789050
paulrwd85@gmail.com <paulrwd85@gmail.com>
2020-09-28 10:40:19 +0300

    B1

--- YandexCup_test/Program.cs
+++ YandexCup_test/Program.cs
@@ -8,13 +8,11 @@ namespace YandexCup_test
         {
             string[] first = Console.ReadLine().Split(' ');
 
-            int win = int.Parse(first[0]);
-            int count = int.Parse(first[1]);
+            int count = int.Parse(first[0]);
+            int period = int.Parse(first[1]);
+            int wakeup = int.Parse(first[2]);
             int[] mas = new int[count];
-            int Vas = 0;
-            int Pet = 0;
-            bool Drw = true;
-
+           
             string[] second = Console.ReadLine().Split(' ');
 
             for (int i = 0; i < count; i++)
@@ -22,48 +20,7 @@ namespace YandexCup_test
                 mas[i] = int.Parse(second[i]);                
             }
 
-            foreach (int i in mas)
-            {
-                if (i % 3 == 0 && i % 5 != 0)
-                {
-                    Pet++;
-                }
-                if (i % 5 == 0 && i % 3 != 0)
-                {
-                    Vas++;
-                }
-                if (Pet == win && Vas == win)
-                {
-                    Console.WriteLine("Draw");
-                    Drw = false;
-                    break;
-                }
-                if (Pet == win)
-                {
-                    Console.WriteLine("Petya");
-                    Drw = false;
-                    break;
-                }
-                if (Vas == win)
-                {
-                    Console.WriteLine("Vasya");
-                    Drw = false;
-                    break;
-                }
-            }
-
-            if(Pet > Vas && Drw)
-            {
-                Console.WriteLine("Petya");
-            }
-            if (Vas > Pet && Drw)
-            {
-                Console.WriteLine("Vasya");
-            }
-            if (Pet == Vas && Drw)
-            {
-                Console.WriteLine("Draw");
-            }
+          
         }
     }
 }
